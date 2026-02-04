3644ea66c2d46d7c9bdb242cab4b9a5aae385b03
Charwak Dongre <charwakdongre@gmail.com>
2026-02-04 11:47:52 +0530

    Fix commit logic to ensure commits are within the past year

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const path = "./data.json";
 
 const markCommit = (x, y) => {
   const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
+    .subtract(1, "y") // Start exactly 1 year ago
+    .add(x, "w") // Add random weeks
+    .add(y, "d") // Add random days
     .format();
 
   const data = {
@@ -23,20 +22,22 @@ const markCommit = (x, y) => {
 };
 
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) return simpleGit().push();
+  const x = random.int(0, 54); // Random week within the past year
+  const y = random.int(0, 6); // Random day within the week
+  const date = moment()
+    .subtract(1, "y") // Start exactly 1 year ago
+    .add(x, "w") // Add random weeks
+    .add(y, "d") // Add random days
+    .format();
 
   const data = {
     date: date,
   };
   console.log(date);
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this, --n));
   });
 };
 
-makeCommits(100
-  
-);
+makeCommits(50);
